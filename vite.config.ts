0d6bf088cf608302b5cbad01daeb539a0eb020f0
Asim Aftab <asim.dev.cs@gmail.com>
2025-11-02 01:12:02 +0530

    Update vite.config.ts (#65)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['salessphere360.com', 'www.salessphere360.com'],
   },
-});
+  define: {
+    'process.env': process.env, // allows .env usage
+  },
+  build: {
+    outDir: 'dist',
+  },
+})
+
+
